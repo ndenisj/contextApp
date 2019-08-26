fdd53d1d35a4b9595107ae1e213aebcb9331ac64
Denis John <denis.john.ndarake@gmail.com>
2019-08-26 20:21:04 +0100

    Used multiple context in one component

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { Component, createContext } from "react";
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 class AuthContextProvider extends Component {
   state = {
